443605567e02dadd2a2bd341b0f47d9ecbc01a72
Matthew Ng <matthewn4444@gmail.com>
2013-12-30 19:43:43 -0500

    Settings actionbar back works now

--- src/com/onscripter/plus/Settings.java
+++ src/com/onscripter/plus/Settings.java
@@ -3,6 +3,7 @@ package com.onscripter.plus;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 
 public final class Settings extends SherlockPreferenceActivity {
 
@@ -12,5 +13,15 @@ public final class Settings extends SherlockPreferenceActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.menu_action_settings);
         addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            break;
+        }
+        return true;
     }
 }
