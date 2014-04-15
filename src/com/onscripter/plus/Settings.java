9e6aab96adba883eecddd6b6f2081d91e591fc58
Matthew Ng <matthewn4444@gmail.com>
2014-04-15 18:32:14 -0400

    You can view the change log in the settings

--- src/com/onscripter/plus/Settings.java
+++ src/com/onscripter/plus/Settings.java
@@ -2,11 +2,14 @@ package com.onscripter.plus;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
 
-public final class Settings extends SherlockPreferenceActivity {
+public final class Settings extends SherlockPreferenceActivity implements OnPreferenceClickListener {
+    private ChangeLog mChangeLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,18 @@ public final class Settings extends SherlockPreferenceActivity {
         getSupportActionBar().setTitle(R.string.menu_action_settings);
         addPreferencesFromResource(R.xml.settings);
         getListView().setBackgroundColor(Color.WHITE);
+
+        Preference preference = getPreferenceScreen().findPreference(getString(R.string.settings_about_change_log_key));
+        preference.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (mChangeLog == null) {
+            mChangeLog = new ChangeLog(this);
+        }
+        mChangeLog.show();
+        return false;
     }
 
     @Override
