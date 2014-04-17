94a5213e0afea568dd281651448364a5c2ae3e85
Matthew Ng <matthewn4444@gmail.com>
2014-04-17 12:26:39 -0400

    Added isshowing function to settings dialog

--- src/com/onscripter/plus/VNSettingsDialog.java
+++ src/com/onscripter/plus/VNSettingsDialog.java
@@ -189,6 +189,10 @@ public class VNSettingsDialog implements OnDismissListener, OnClickListener, OnS
         mDialog.show();
     }
 
+    public boolean isShowing() {
+        return mDialog.isShowing();
+    }
+
     @Override
     public void onTabChanged(String tabId) {
         if (tabId.equals(TEXT_TAB_NAME)) {
