df6c18e9258c19c91c7b915770cd61d21dabe0e4
Matthew Ng <matthewn4444@gmail.com>
2014-07-02 14:21:34 -0400

    Checks if last directory exists before using it

--- src/com/onscripter/plus/LauncherActivity.java
+++ src/com/onscripter/plus/LauncherActivity.java
@@ -97,6 +97,12 @@ public class LauncherActivity extends SherlockActivity implements AdapterView.On
             }
         } else {
             directory = new File(lastDirectory);
+            if (!directory.exists()) {
+                directory = getStartingDirectory();
+                if (directory == null) {
+                    return;
+                }
+            }
         }
 
         // Set up the listView and the adapter
