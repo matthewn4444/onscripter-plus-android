e8df68a91e93cac2650a48e2bacdbfee3c245ab3
Matthew Ng <matthewn4444@gmail.com>
2014-11-24 00:30:46 -0800

    Fixes the changelog not showing up on devices that have no write issues

--- src/com/onscripter/plus/ExtSDCardFix.java
+++ src/com/onscripter/plus/ExtSDCardFix.java
@@ -76,10 +76,11 @@ public final class ExtSDCardFix {
                 && getNumONScripterGames(currentPath).length > 0;
     }
 
-    public ExtSDCardFix(Activity activity, FileSystemAdapter adapter) {
+    public ExtSDCardFix(Activity activity, FileSystemAdapter adapter, OnSDCardFixListener listener) {
         mActivity = activity;
         mAdapter = adapter;
         mPrefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        mListener = listener;
 
         // Don't rescan if already scanned
         synchronized (ExtSDCardFix.this) {
@@ -101,10 +102,10 @@ public final class ExtSDCardFix {
            sExtSDCardWritable = Environment2.hasExternalSDCard();
            sAlreadyScanned = true;
        }
-    }
 
-    public void setOnSDCardFixListener(OnSDCardFixListener listener) {
-        mListener = listener;
+       if (mListener != null) {
+           mListener.writeTestFinished();
+       }
     }
 
     /**
