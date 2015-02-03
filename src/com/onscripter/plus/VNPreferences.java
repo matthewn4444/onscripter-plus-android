bcb3605386da3b0d491366760ba0bb7a0336cc9d
Matthew Ng <matthewn4444@gmail.com>
2015-02-02 23:32:48 -0800

    If statfs fails to read folder on VNPref save, ignore it and dont crash

--- src/com/onscripter/plus/VNPreferences.java
+++ src/com/onscripter/plus/VNPreferences.java
@@ -372,9 +372,21 @@ public class VNPreferences {
                 break;
             case SAVE_TASK:
                 // Check for space left
-                StatFs stat = new StatFs(mPath);
-                long bytesAvailable = (long)stat.getBlockSize() *(long)stat.getBlockCount();
-                final long kbAvailable = bytesAvailable / 1024;
+                long _kbAvailable = 0;
+                try {
+                    StatFs stat = new StatFs(mPath);
+                    long bytesAvailable = (long)stat.getBlockSize() *(long)stat.getBlockCount();
+                    _kbAvailable = bytesAvailable / 1024;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    final HashMap<String, String> passArgs = new HashMap<String, String>(){{
+                        put("Path", mPath);
+                        put("Ext SDcard Writable", ExtSDCardFix.isWritable() + "");
+                    }};
+                    BugSenseHandler.sendExceptionMap(passArgs, e);
+                    return OnLoadVNPrefListener.Result.NO_ISSUES;
+                }
+                final long kbAvailable = _kbAvailable;
 
                 // If at least 300kb is left over
                 if (kbAvailable <= MIN_FILE_SPACE_KB) {
